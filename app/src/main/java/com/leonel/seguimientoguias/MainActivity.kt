10ae1d27f710bd03a8486b615e79d9fec942f00c
lupamu14 <leonel.pacheco@gmail.com>
2023-05-30 12:48:36 -0600

    [ADD] Se agrega navegación y primeras pantallas

--- app/src/main/java/com/leonel/seguimientoguias/MainActivity.kt
+++ app/src/main/java/com/leonel/seguimientoguias/MainActivity.kt
@@ -2,10 +2,23 @@ package com.leonel.seguimientoguias
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/leonel/seguimientoguias/ui/home/login/LoginFragment.kt
+++ app/src/main/java/com/leonel/seguimientoguias/ui/home/login/LoginFragment.kt
@@ -1,22 +1,16 @@
-package com.leonel.designandroid.ui.home
+package com.leonel.seguimientoguias.ui.home.login
 
 import android.os.Bundle
-import android.transition.AutoTransition
-import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.leonel.seguimientoguias.R
 import com.leonel.seguimientoguias.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -30,17 +24,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(LoginViewModel::class.java)
 
 
         // var rootView = inflater.inflate(R.layout.fragment_home, container, false)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root //TODO tu codigo va bajo este
 
-        binding.button.text="Continuar"
+        binding.button.text="Continuar desde login"
 
         binding.button.setOnClickListener {
-
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
 
         // val textView: TextView = binding.textHome
